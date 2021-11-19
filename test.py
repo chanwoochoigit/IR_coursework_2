4df9d950842679b15a0e8c479b4c8777508e75c0
chanwoochoigit <ccwpoint@gmail.com>
2021-11-19 22:47:43 +0000

    part 2 almost done

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 nums = [3,3,3,3,2,2,2,2,1,1]
 nums_2 = [0,1,0,0,1,0,0,0,0,1]
 from math import log2, pow
@@ -38,5 +40,25 @@ def calc_mi(N11, N10, N01, N00):
 def calc_chi(N11, N10, N01, N00):
     return ((N11 + N10 + N01 + N00) * pow(((N11 * N00) - (N10 * N01)),2))/((N11+N01)*(N11+N10)*(N10+N00)*(N01+N00))
 
-print(calc_mi(2, 0, 20754, 12728))
-print(calc_chi(2, 0, 20754, 12728))
+def test_rand():
+    for i in range(1000):
+        n = randrange(1,21)
+        if n == 20:
+            print(n)
+
+def extract(string):
+    ids = []
+    aa = string.replace(' ','').replace('\"','').split('+')
+    for a in aa:
+        num = a.split('*')
+        ids.append(num)
+
+    print(ids)
+
+# print(calc_mi(2, 0, 20754, 12728))
+# print(calc_chi(2, 0, 20754, 12728))
+# test_rand()
+sample_str = '0.132 * "3" + 0.107 * "85" + 0.076 * "662" + 0.067 * "438" + 0.039 * "654" + 0.033 * "651" + 0.033 * "161" +' \
+             ' 0.030 * "1707" + 0.023 * "1855" + 0.019 * "841"'
+
+extract(sample_str)
