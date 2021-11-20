a28a27b058a74e917c68ccc9c9af2ac666ebcfca
chanwoochoigit <ccwpoint@gmail.com>
2021-11-20 16:43:26 +0000

    created count matrix for svm classifier

--- analyse.py
+++ analyse.py
@@ -4,11 +4,15 @@ import sys
 from collections import defaultdict
 import json
 from random import randrange
+
+import numpy as np
 from gensim.corpora.dictionary import Dictionary
 from gensim.test.utils import datapath
 from gensim.models import LdaModel
 from nltk.stem import PorterStemmer
 from math import log2
+from scipy import sparse
+
 #my preprocessing module from coursework 1
 class Preprocessor():
 
@@ -24,26 +28,31 @@ class Preprocessor():
         items_1d = list(itertools.chain.from_iterable(items))
         unique_dump = []
         [unique_dump.append(x) for x in items_1d if x not in unique_dump]
+        vocab = {}
+        for i, word in enumerate(unique_dump):
+            vocab[i] = word
 
-        return unique_dump
+        return vocab
 
-    def count_frequencies(self, data, unique_words):
-        word_names = []
-        word_counts = []
-        check_count = 0
+    def create_count_matrix(self, docs, vocab):
+        count_mtx = sparse.dok_matrix((len(docs), len(vocab)), dtype=int)
 
-        for word in unique_words:
-            count = 0
-            word_names.append(word)
-            for word_to_compare in data:
-                if len(word_to_compare) == len(word):
-                    if word_to_compare == word:
-                        count += 1
-            word_counts.append(count)
-            check_count += 1
-            if check_count % 100 == 1:
-                print('counting word number ... {}/{}'.format(check_count, len(unique_words)))
-        word_freq = {"Word": word_names, "Count": word_counts}
+        for v_ in vocab.keys():
+            if v_ % 10 == 0:
+                print('counting word number ... {}%'.format(round(v_ / len(vocab) * 100, 3)))
+            voca = vocab[v_]
+            for d_ in docs.keys():
+                counter = 0
+                for word in docs[d_]:
+                    if voca == word:
+                        counter += 1
+                count_mtx[d_, v_] = counter
+
+        #convert to coo and save as npz because dok save is not available yet
+        #convert back to dok after loading
+        sparse.save_npz('count_matrix.npz', count_mtx.tocoo())
+
+        return count_mtx
 
     def trim_text(self, text):
         text_str = text.replace('\n', ' ').replace('\t',' ').replace('  ',' ')  # replace \n with a space, and if that creates a double space, replace it with a single space
@@ -77,6 +86,18 @@ class Preprocessor():
 
         return clean
 
+    def preprocess_baseline(self, data_chunk):
+        # trim
+        text_str = self.trim_text(data_chunk)
+
+        # tokenise
+        words_dup = self.tokenise(text_str)
+
+        return words_dup
+
+    # def create_vocab_vector(self, vocab, words):
+
+
     #preprocess 1-d list of text
     def preprocess(self, data_chunk):
         #trim
@@ -108,7 +129,6 @@ class Analyse():
 
     def __init__(self):
         self.corpus = self.load_corpus()
-        self.lda = self.load_lda()
         self.p = Preprocessor()
 
     def init_nd_dict(self):
@@ -309,7 +329,7 @@ class Analyse():
         len_ot, len_nt, len_qu = len(self.corpus['ot'].keys()), len(self.corpus['nt'].keys()), len(self.corpus['quran'].keys())
         lda_result_dict = self.init_nd_dict()
 
-        lda_distrib = self.lda.get_document_topics(self.get_lda_corpus())
+        lda_distrib = self.load_lda().get_document_topics(self.get_lda_corpus())
 
         #add results for each corpus to get average score for each topic
         for i, line in enumerate(lda_distrib):
@@ -386,13 +406,13 @@ class Analyse():
 
         #find key tokens for each corpus
 
-        lda_token_str_ot = self.lda.print_topic(int(ot_topic_best))
+        lda_token_str_ot = self.load_lda().print_topic(int(ot_topic_best))
         power_words_ot = self.extract_tokens_from_lda_str(lda_token_str_ot)
 
-        lda_token_str_nt = self.lda.print_topic(int(nt_topic_best))
+        lda_token_str_nt = self.load_lda().print_topic(int(nt_topic_best))
         power_words_nt = self.extract_tokens_from_lda_str(lda_token_str_nt)
 
-        lda_token_str_qu = self.lda.print_topic(int(qu_topic_best))
+        lda_token_str_qu = self.load_lda().print_topic(int(qu_topic_best))
         power_words_qu = self.extract_tokens_from_lda_str(lda_token_str_qu)
 
         print(power_words_ot)
@@ -401,6 +421,33 @@ class Analyse():
 
         return ot_topic_best, nt_topic_best, qu_topic_best
 
+class Classifier():
+
+    def __init__(self):
+        pass
+
+    def prepare_data(self):
+        p = Preprocessor()
+
+        with open('train_and_dev.tsv', 'r') as f:
+            raw_text = f.readlines()
+
+        docs = {}
+        labels = []
+        for docid, line in enumerate(raw_text):
+            if docid % 5000 == 0:
+                print('building docs and preprocessing...{}%'.format(round(docid / len(raw_text) * 100, 2)))
+            c, text = line.split('\t')
+            docs[docid] = p.preprocess_baseline(text)
+            labels.append(c)
+
+        vocab = p.unique_from_array(list(docs.values()))
+        count_mtx = p.create_count_matrix(docs, vocab)
+
+    def load_cm(self):
+        # convert back to dok after loading
+        coo = sparse.load_npz('count_matrix.npz')
+        return coo.todok()
 
 a = Analyse()
 # corp = a.create_corpus()
@@ -414,3 +461,6 @@ a = Analyse()
 # a.train_lda(k=20)
 # a.lda_calc_average_score()
 # a.find_top_tokens()
+c = Classifier()
+# c.prepare_data()
+print(c.load_cm())
